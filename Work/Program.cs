40fdba3abbd3a8f1322ebe71d03cf7009a670ac5
LarsonRagnar <larionov1992@gmail.com>
2023-03-22 20:46:33 +0300

    ПРограмма готова

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,7 +1,22 @@
-﻿Console.WriteLine("Ввведите что то ");
+﻿ Console.WriteLine("Ввведите что то ");
 string input = Console.ReadLine();
 string[] strings = new string[input.Length];
 for (int i = 0; i < input.Length; i++)
 {
     strings[i] = input[i].ToString();
 }
+int randNumArr=0;
+Random rnd = new Random();
+int randSizeArr=0;
+randSizeArr=rnd.Next(4);
+string[] stringsThree = new string[randSizeArr];
+
+for (int i = 0; i < stringsThree.Length; i++)
+{   
+    randNumArr = rnd.Next(strings.Length);
+    stringsThree[i]=strings[randNumArr];  
+}
+
+
+Console.WriteLine((string.Join(", ", strings)));
+Console.WriteLine(string.Join(", ", stringsThree));
